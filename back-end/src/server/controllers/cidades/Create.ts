23c9dd784708afb0440f8df968d93cf94cfc1b4f
Adelson Barros Dos Santos <40273131+adelson101@users.noreply.github.com>
2024-06-17 16:36:16 -0300

    generating message erros

--- back-end/src/server/controllers/cidades/Create.ts
+++ back-end/src/server/controllers/cidades/Create.ts
@@ -1,11 +1,37 @@
 import { Request, Response } from "express";
+import * as yup from "yup";
+import StatusCode from "http-status-codes"
 
 interface ICidade {
-    nome: string
+    nome: string,
+    estado: string
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-    console.log(req.body.nome);
+const BodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3),
+    estado: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    let validatedData: ICidade | undefined = undefined;
+
+    try {
+        validatedData = await BodyValidation.validate(req.body, { abortEarly: false });
+    } catch (err) {
+
+        const yupError = err as yup.ValidationError;
+        const ValidatorError: Record<string,string> = {};
+
+        yupError.inner.forEach(error => {
+            if (error.path === undefined) return;
+            ValidatorError[error.path] = error.message;
+        });
+
+        return res.status(StatusCode.BAD_REQUEST).json({ errors: ValidatorError });
+    }
+
+    console.log(validatedData);
     
+
     return res.send('Create');
 }
