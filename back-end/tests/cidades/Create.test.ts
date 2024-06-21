c9a7711fb0e80937b3965d65a21ceadf57a48239
Adelson Barros Dos Santos <40273131+Adelson10@users.noreply.github.com>
2024-06-21 16:37:11 -0300

    up

--- back-end/tests/cidades/Create.test.ts
+++ back-end/tests/cidades/Create.test.ts
@@ -2,11 +2,8 @@ import { StatusCodes } from "http-status-codes";
 import { testServer } from "../jest.setup";
 
 describe('Create - Cidade', () => {
-    it('Criar Registro', async () => {
-        const res1 = await testServer
-        .post('/cidades')
-        .send({ nome: 'Colinas' });
-
-        expect(res1.statusCode).toEqual(StatusCodes.CREATED);
+    test('Criar Registro ', async () => {
+        const res = await testServer.post('/cidades').send({nome: "Colinas"});
+        expect(res.statusCode).toEqual(StatusCodes.CREATED);
     });
 });
