2a1dba9e21d25c925d50134454ba69588526a2c8
Adelson Barros Dos Santos <40273131+Adelson10@users.noreply.github.com>
2024-06-24 10:04:27 -0300

    up

--- back-end/tests/cidades/DeleteById.test.ts
+++ back-end/tests/cidades/DeleteById.test.ts
@@ -3,9 +3,9 @@ import { testServer } from '../jest.setup';
 
 describe('Cidades - DeleteById', () => {
     test('Apaga registro', async () => {
-      const res = await testServer.delete('/cidades').send({nome:"Colinas"});
+      const res = await testServer.post('/cidades').send({nome:"Colinas do Tocantins"});
       expect(res.statusCode).toEqual(StatusCodes.CREATED);
-
+      
       const resApagada = await testServer.delete(`/cidades/${res.body}`).send();
       expect(resApagada.statusCode).toEqual(StatusCodes.NO_CONTENT);
     });
