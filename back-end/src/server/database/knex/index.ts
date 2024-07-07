9d27e5498ca4b31250fe8c035d60392e3eb43fc3
Adelson Barros Dos Santos <40273131+Adelson10@users.noreply.github.com>
2024-07-07 19:34:56 -0300

    update

--- back-end/src/server/database/knex/index.ts
+++ back-end/src/server/database/knex/index.ts
@@ -4,7 +4,9 @@ import pg from 'pg';
 
 import { development, production, test } from './Environment';
 
-
+if(process.env.NODE_ENV === 'production') {    
+    pg.types.setTypeParser( 20, 'text', parseInt );
+}
 
 const getEnvironment = () => {    
     switch (process.env.NODE_ENV) {
