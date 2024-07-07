b8130c490f80bf6ad62a3f2f5b2c77141b1c673e
Adelson Barros Dos Santos <40273131+Adelson10@users.noreply.github.com>
2024-07-07 15:22:08 -0300

    update

--- back-end/src/server/database/knex/Environment.ts
+++ back-end/src/server/database/knex/Environment.ts
@@ -27,5 +27,25 @@ export const test: Knex.Config = {
 };
 
 export const production: Knex.Config = {
-    ...development,
+    client: 'pg',
+    migrations: {
+        directory: path.resolve(__dirname, '..', 'migrations')
+    },
+    seeds: {
+        directory: path.resolve(__dirname, '..', 'seeds')
+    },
+    pool: {
+        afterCreate: (connection: any, done: Function) => {
+            connection.run('PRAGMA foreign_keys = ON');
+            done();
+        }
+    },
+    connection: {
+        host: process.env.DATABASE_HOST,
+        user:process.env.DATABASE_USER,
+        database: process.env.DATABASE_NAME,
+        password: process.env.DATABASE_PASSWORD,
+        port: Number(process.env.DATABASE_PORT || 5432),
+        ssl: { rejectUnauthorized: false },
+    },
 };
