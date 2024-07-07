5e9baec5139a807e73a7f4d78a85d38df9e76fc9
Adelson Barros Dos Santos <40273131+Adelson10@users.noreply.github.com>
2024-07-07 19:26:01 -0300

    up

--- back-end/src/server/database/knex/index.ts
+++ back-end/src/server/database/knex/index.ts
@@ -5,6 +5,8 @@ import pg from 'pg';
 import { development, production, test } from './Environment';
 
 if(process.env.NODE_ENV === 'production') {
+    console.log('deu certo');
+    
     pg.types.setTypeParser( 20, 'text', parseInt );
 }
 
